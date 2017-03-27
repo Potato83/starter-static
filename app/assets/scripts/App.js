b166ee7a34cdae2290a4c195e2095041e1eb62fc
Sam Vipond <sammy.potato@gmail.com>
2017-03-27 18:25:46 -0400

    first commit

---
+++ app/assets/scripts/App.js
@@ -0,0 +1,15 @@
+import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
+import StickyHeader from './modules/StickyHeader';
+import $ from 'jquery';
+import Modal from './modules/Modal';
+
+var mobileMenu = new MobileMenu();
+new RevealOnScroll($('.feature-item'), "85%");
+new RevealOnScroll($('.testimonial'), "60%");
+new RevealOnScroll($('.reveal'), "70%");
+var stickyHeader = new StickyHeader();
+var modal = new Modal();
+
+
+
